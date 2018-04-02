bfba8d0c7b1fe3009e81eaa65361a872188e15f2
Daniel Roberts <DanielRobe@datacom.co.nz>
2018-04-03 09:05:07 +1200

    Updated to page 388

---
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConfiguringApps.Infrastructure;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+
+        public HomeController(UptimeService up)
+        {
+            uptime = up;
+        }
+
+
+        public ViewResult Index()
+            => View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Index action",
+                ["Uptime"] = $"{uptime.Uptime}ms"
+            });
+    }
+}
